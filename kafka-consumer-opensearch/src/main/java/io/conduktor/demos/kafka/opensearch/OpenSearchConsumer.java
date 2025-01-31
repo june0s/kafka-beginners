761f3f8022de874b90b92303569573b9794de5dd
june0o <cucucy@naver.com>
2025-01-31 10:35:29 +0900

    add openSearchClient and create index

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -1,8 +1,84 @@
 package io.conduktor.demos.kafka.opensearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.CreateIndexResponse;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class OpenSearchConsumer {
 
-    public static void main(String[] args) {
+    public static RestHighLevelClient createOpenSearchClient() throws URISyntaxException {
+        String connString = "http://localhost:9200";
 
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = new URI(connString);
+
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder
+                                            .setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                            )
+            );
+        }
+        return restHighLevelClient;
+    }
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+
+        // first create an OpenSearch client
+        final RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        // try 문에 openSearchClient를 전달하면, openSearchClient.close() 가 호출된다고 한다. java magic!
+        try (openSearchClient) {
+            String index = "wikimedia";
+
+            final boolean indexExists = openSearchClient.indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT);
+            if (!indexExists) {
+                // we need to create the index on OpenSearch if it doesn't exist already
+                final CreateIndexRequest createIndexRequest = new CreateIndexRequest(index);
+                final CreateIndexResponse response = openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                log.info("The Wikimedia index has been created. ack = " + response.isAcknowledged());
+            } else {
+                log.info("The Wikimedia index already exists.");
+            }
+        }
+
+        // create our Kafka client
+
+        // main code logic
+
+        // close things
     }
 }
